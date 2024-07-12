385fdba916fb7b17fe9196c7fa96f76a15ce86f1
Zenisis <119743135+Zenisis@users.noreply.github.com>
2024-07-12 10:06:20 +0530

    Update middlewares.js

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -10,3 +10,12 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
+// config/middlewares.js
+module.exports = ({ env }) => ({
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['http://localhost:3000', 'http://localhost:1337'], // Add your React app's URL and the Strapi server URL here
+    },
+  },
+});
